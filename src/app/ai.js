8001cf05b6bd4f72fff63ded7770e914c91a93c1
Md. Raiyan Takrim <raiyan.takrim3@gmail.com>
2024-12-18 00:43:58 +0800

    Update Hugging Face API key environment variable for AI component

--- src/app/ai.js
+++ src/app/ai.js
@@ -1,6 +1,6 @@
 'use server'
 import { HfInference } from '@huggingface/inference';
-const hf = new HfInference(process.env.HUGGINGFACE_API_KEY);
+const hf = new HfInference(process.env.NEXT_PUBLIC_HUGGINGFACE_API_KEY);
 
 const system_prompt = `You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mentioned in your recipe. The recipe can include additional ingredients they didn't mention, but try to not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page`;
 
